ca637902306b4a2033f90702c99deb189046d223
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-03 01:39:48 +0200

    use `path-exists` module as `fs.exists` is being deprecated
    https://github.com/sindresorhus/path-exists

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
-var fs = require('fs');
 var fileUrl = require('file-url');
 var prependHttp = require('prepend-http');
+var pathExists = require('path-exists');
 
 module.exports = function (url) {
 	if (typeof url !== 'string') {
 		throw new TypeError('Expected a string');
 	}
 
-	return fs.existsSync(url) ? fileUrl(url) : prependHttp(url);
+	return pathExists.sync(url) ? fileUrl(url) : prependHttp(url);
 };
