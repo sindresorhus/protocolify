1895c3784bd106868bfdbe788f3b2a5a1dbfab74
Sindre Sorhus <sindresorhus@gmail.com>
2016-05-17 00:08:53 +0700

    ensure it doesn't modify data URLs

--- test.js
+++ test.js
@@ -4,4 +4,7 @@ import fn from './';
 test(t => {
 	t.is(fn('todomvc.com'), 'http://todomvc.com');
 	t.true(/file:\/\/.*index\.js/.test(fn('index.js')));
+
+	const dataURL = 'data:image/png;base64,iVBORw0KGgo=';
+	t.is(fn(dataURL), dataURL);
 });
